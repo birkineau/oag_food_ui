f94d62cda3206726d3f08348407d3975f0eb0e49
Orlys Azaharez Garcia <orlys.azaharez@lacoloniamedical.com>
2024-04-24 10:22:05 -0400

    force portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:oag_food_ui/food_application.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
   runApp(
     const MaterialApp(
       home: Scaffold(
